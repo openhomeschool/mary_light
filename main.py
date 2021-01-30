3963b4bf05e357482019f59f41df1677313b41d7
openhomeschool <nospam6@doubleserver.com>
2021-01-29 22:41:30 -0800

    put leds and notes into lists, added constants, cleaned up

--- main.py
+++ main.py
@@ -11,43 +11,26 @@ DURATION_MULTIPLIER = 0.2
 REST = 0.1
 
 # Light
-def light(led, duration):
-	led.on()
+def light(led_number, duration):
+	led_index = led_number - 1
+	leds[led_index].on()
 	sleep(duration * DURATION_MULTIPLIER)
-	led.off()
-	sleep(RESAT)
+	leds[led_index].off()
+	sleep(REST)
 
 # Mary Had A Little Lamb:
-notes = [ # value, duration
-					(3, 1),
-					(2, 1),
-					(1, 1),
-					(2, 1),
-					(3, 1),
-					(3, 1),
-					(3, 2),
-					(2, 1),
-					(2, 1),
-					(2, 2),
-					(3, 1),
-					(3, 1),
-					(3, 2),
-					(3, 1),
+notes = [ 
+		# Each tuple is (led_number, duration); a duration of 1 is a quarter note; 2 = half note, 4 = whole note
+		(3, 1), (2, 1), (1, 1), (2, 1), (3, 1), (3, 1), (3, 2),
+		(2, 1), (2, 1), (2, 2),
+		(3, 1), (3, 1), (3, 2),
 
-					(3, 1),
-					(2, 1),
-					(1, 1),
-					(2, 1),
-					(3, 1),
-					(3, 1),
-					(3, 1),
-					(3, 1),
-					(2, 1),
-					(2, 1),
-					(3, 1),
-					(2, 1),
-					(1, 4),
+		(3, 1), (2, 1), (1, 1), (2, 1), (3, 1), (3, 1), (3, 1),
+		(3, 1), (2, 1), (2, 1), (3, 1), (2, 1), (1, 4),
 	]
 
 for led, duration in notes:
 	light(led, duration)
+
+sleep(1) # take a bow
+
